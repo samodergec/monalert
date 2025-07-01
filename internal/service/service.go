5a4826cfe677317c1e7f6f0f49fa3f2881c5c400
Aleksei Romanov <samodergec@users.noreply.github.com>
2025-07-01 22:09:08 +0400

    ITER-3
    Two methods extends repository interface in service layer
    Use correct names for fields in requests for gauge and counter update in repository

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,12 +1,16 @@
 package service
 
 import (
+	"fmt"
+	"log"
 	"monalert/internal/repository"
 )
 
 type Repository interface {
 	GaugeUpdate(req *repository.GaugeUpdateRequest)
 	CounterUpdate(req *repository.CounterUpdateRequest)
+	GetMetricValue(req *repository.GetMetricValueRequest) (*repository.GetMetricValueResponse, error)
+	GetAllMetrics() []string
 }
 
 type Monalert struct {
@@ -31,14 +35,48 @@ type CounterUpdateRequest struct {
 
 func (m *Monalert) GaugeUpdate(req *GaugeUpdateRequest) {
 	m.store.GaugeUpdate(&repository.GaugeUpdateRequest{
-		Metric: req.Metric,
-		Value:  req.Value,
+		MetricName:  req.Metric,
+		MetricValue: req.Value,
 	})
 }
 
 func (m *Monalert) CounterUpdate(req *CounterUpdateRequest) {
 	m.store.CounterUpdate(&repository.CounterUpdateRequest{
-		Metric: req.Metric,
-		Value:  req.Value,
+		MetricName:  req.Metric,
+		MetricValue: req.Value,
 	})
+}
+
+type GetMetricValueRequest struct {
+	MetricType string
+	MetricName string
+}
+
+type GetMetricValueResponse struct {
+	MetricValue string
+}
+
+func (m *Monalert) GetMetricValue(req *GetMetricValueRequest) (*GetMetricValueResponse, error) {
+	log.Printf("service: request: %+v", req)
+
+	res, err := m.store.GetMetricValue(&repository.GetMetricValueRequest{
+		MetricType: req.MetricType,
+		MetricName: req.MetricName,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("service: failed to get metric value: %w", err)
+	}
+	log.Printf("service: got value from repo: %+v", res)
+
+	return &GetMetricValueResponse{
+		MetricValue: res.MetricValue,
+	}, nil
+}
+
+func (m *Monalert) GetAllMetrics() []string {
+	metrics := m.store.GetAllMetrics()
+	if len(metrics) == 0 {
+		return []string{}
+	}
+	return metrics
 }
