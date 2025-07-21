1bfd0ee852aed956efdccdf90be284b54c25f2e2
Aleksei Romanov <samodergec@users.noreply.github.com>
2025-07-21 22:05:01 +0400

    iter5
    env parameters in agent to define server ADDRESS, REPORT_INTERVAL, POLL_INTERVAL

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,15 +1,32 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
+
+type Config struct{
+	
+}
 
 var flagServerAddr string
 var flagReportInterval int
 var flagPollInterval int
-
 
 func parseFlags() {
 	flag.StringVar(&flagServerAddr, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&flagReportInterval, "r", 2, "interval for metric send")
 	flag.IntVar(&flagPollInterval, "p", 1, "interval for collecting metrics")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagServerAddr = envRunAddr
+	}
+	if envReportInterval, _ := strconv.Atoi(os.Getenv("REPORT_INTERVAL")); envReportInterval != 0 {
+		flagReportInterval = envReportInterval
+	}
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		flagServerAddr = envPollInterval
+	}
 }
